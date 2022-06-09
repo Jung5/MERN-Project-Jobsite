a6009e1fe0c823fe9b7d27ddd6a99131536eada4
JeongwookNa <najung936@hotmail.com>
2022-06-09 12:52:14 -0500

    using styledcomponent

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import Home from "./components/Home";
 
 const App = () => {
   return (
-    <div> Test</div>
-    );
+    <>
+      <Home />
+    </>
+  );
 };
 
 export default App;
