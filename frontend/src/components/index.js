db7c0b8ff6785894e11b55be56502a37483a45b9
JeongwookNa <najung936@hotmail.com>
2022-06-09 13:49:24 -0500

    basic routes setup

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,3 +1,7 @@
 import Logo from "./Logo";
+import Error from "./Error";
+import Register from "./Register";
+import Home from "./Home";
+import Dashboard from "./Dashboard";
 
-export { Logo }
+export { Dashboard, Logo, Home, Register, Error };
