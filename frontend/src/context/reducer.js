03419d33679f3339606dc9b75e4bc9519afac275
JeongwookNa <najung936@hotmail.com>
2022-06-11 16:33:58 -0500

    throws alert when login information is missing

--- frontend/src/context/reducer.js
+++ frontend/src/context/reducer.js
@@ -1,6 +1,23 @@
 import React from "react";
+import { DISPLAY_ALERT, CLEAR_ALERT } from "./actions";
 
 const reducer = (state, action) => {
+  if (action.type === DISPLAY_ALERT) {
+    return {
+      ...state,
+      showAlert: true,
+      alertType: "danger",
+      alertText: "Please enter your login information!",
+    };
+  }
+  if (action.type === CLEAR_ALERT) {
+    return {
+      ...state,
+      showAlert: false,
+      alertType: "",
+      alertText: "",
+    };
+  }
   throw new Error(`no such action : ${action.type}`);
 };
 
