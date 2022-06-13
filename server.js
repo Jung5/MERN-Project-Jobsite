69f45ca413736613b8d3cecdd5e7bbb92d0d4f10
JeongwookNa <najung936@hotmail.com>
2022-06-13 16:01:50 -0500

    setup Mongo

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
-console.log('server running');
+import express from "express";
+const app = express();
+
+import dotenv from "dotenv";
+dotenv.config();
+
+import connectDB from './db/connect.js'
+
+// Middleware
+import notFoundMiddleware from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+
+app.get("/", (req, res) => {
+  res.send("Welcome!");
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+// PORT
+const port = process.env.PORT || 5000;
+
+const start = async () =>{
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}...`);
+          });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
