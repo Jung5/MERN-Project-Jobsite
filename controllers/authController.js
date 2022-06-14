2a15cfd00b30e1f6f1bd39c872369082481a5ca0
JeongwookNa <najung936@hotmail.com>
2022-06-14 03:45:35 -0500

    intial setup regController done

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,19 +1,17 @@
-import User from '../models/User.js'
+import User from "../models/User.js";
+import { StatusCodes } from "http-status-codes";
 
 const register = async (req, res, next) => {
-  try {
-    const user = await User.create(req.body)
-    res.status(201).json({user})
-  } catch (error) {
-    next(error)
-  };
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ user });
 };
+
 const login = async (req, res) => {
   res.send("login user");
 };
+
 const updateUser = async (req, res) => {
   res.send("update user");
 };
 
 export { register, login, updateUser };
- 
