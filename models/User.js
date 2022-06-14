21b9eee94f6405151f764359d4863a0cf9eecbdc
JeongwookNa <najung936@hotmail.com>
2022-06-14 03:14:22 -0500

    validator was added

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import validator from "validator";
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -11,6 +12,10 @@ const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, "Please provide email"],
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide a valid email",
+    },
     unique: true,
   },
   password: {
@@ -22,13 +27,15 @@ const UserSchema = new mongoose.Schema({
     type: String,
     trim: true,
     maxlength: 20,
-    default: 'lastName'
+    default: "lastName",
   },
   location: {
     type: String,
     required: [true, "Please provide password"],
     trim: true,
     maxlength: 20,
-    default: 'my city'
+    default: "my city",
   },
 });
+
+export default mongoose.model("User", UserSchema);
