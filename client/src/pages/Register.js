c1b4dce1643a9c5817e2547affe1f894a43aeae4
JeongwookNa <najung936@hotmail.com>
2022-06-15 13:37:32 -0500

    localstorage token available

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { FormRow, Logo, Alert } from "../components/index";
 import { useAppContext } from "../context/appContext";
 import Wrapper from "../Wrappers/RegisterPage";
@@ -11,27 +12,45 @@ const initialState = {
 };
 
 const Register = () => {
+  const navigate = useNavigate();
   const [values, setValues] = useState(initialState);
-
-  const { isLoading, showAlert, displayAlert } = useAppContext();
+  const { user, isLoading, showAlert, displayAlert, registerUser } =
+    useAppContext();
 
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember });
   };
 
   const handleChange = (e) => {
-    setValues({...values, [e.target.name]:e.target.value})
+    setValues({ ...values, [e.target.name]: e.target.value });
   };
 
   const onSubmit = (e) => {
     e.preventDefault();
-    const {name, email, password, isMember} = values
-    if(!email || !password || (!isMember && !name)){
-      displayAlert()
-      return
+    const { name, email, password, isMember } = values;
+    if (!email || !password || (!isMember && !name)) {
+      displayAlert();
+      return;
+    }
+
+    const currentUser = { name, email, password };
+    if (isMember) {
+      console.log("already a member");
+    } else {
+      registerUser(currentUser);
     }
     console.log(values);
   };
+
+
+useEffect(()=> {
+  if(user) {
+    setTimeout(() => {
+      navigate('/')
+    }, 3000)
+  }
+}, [user, navigate])
+
 
   return (
     <Wrapper className="full-page">
@@ -59,7 +78,7 @@ const Register = () => {
           value={values.password}
           handleChange={handleChange}
         />
-        <button type="submit" className="btn btn-block">
+        <button type="submit" className="btn btn-block" disabled={isLoading}>
           Submit
         </button>
         <p>
