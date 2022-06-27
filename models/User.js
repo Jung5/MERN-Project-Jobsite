7adfcdbd8c0961d5d9abcf320952a49e69202cd6
JeongwookNa <najung936@hotmail.com>
2022-06-27 17:55:28 -0500

    changing workstation

--- models/User.js
+++ models/User.js
@@ -56,5 +56,4 @@ UserSchema.methods.comparePassword = async function (candidatePassword) {
   const isMatch = await bcrypt.compare(candidatePassword, this.password);
   return isMatch;
 };
-
 export default mongoose.model("User", UserSchema);
